d050b466f615fc9a96338a0a699e73b8cdc5f3d0
Leeleo3x <leeleo3x@gmail.com>
2019-11-24 11:28:52 -0500

    checkpoint.

---
+++ Assets/UpdateBloodPos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateBloodPos : MonoBehaviour
+{
+
+    public Transform objectToFollow;
+    public float y;
+    // Update is called once per frame
+
+    void LateUpdate()
+    {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        ParticleSystem.ShapeModule editableShape = ps.shape;
+        var pos = -objectToFollow.position;
+        editableShape.position = new Vector3(pos.x, y, pos.z);
+        Debug.Log(objectToFollow.position);
+        Debug.Log(transform.position);
+    }
+}
---
+++ Assets/UpdatePlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdatePlayer : MonoBehaviour
+{
+
+    public Transform objectToFollow;
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(objectToFollow.position.x, transform.position.y, objectToFollow.position.z);
+    }
+}
