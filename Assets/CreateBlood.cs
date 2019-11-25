6ab740cc3c76efe38b7f4367c4a65f0c4c1debe4
Leeleo3x <leeleo3x@gmail.com>
2019-11-25 01:20:29 -0500

    udpate.

--- Assets/CreateBlood.cs
+++ Assets/CreateBlood.cs
@@ -8,7 +8,7 @@ public class CreateBlood : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(blood, new Vector3(0, 0, 0), Quaternion.identity);
+        // Instantiate(blood, new Vector3(0, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
